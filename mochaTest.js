b94284f0f91d721d7ed0b6a923f5d074d1d0e907
Marc d'Entremont <cramhead@gmail.com>
2015-01-03 16:29:05 -0800

    add browser-policy package and add policies to allow framing, but it does not work

--- mochaTest.js
+++ mochaTest.js
@@ -1,23 +1,44 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+	// counter starts at 0
+	Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+	Template.hello.helpers({
+		counter: function() {
+			return Session.get("counter");
+		}
+	});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+	Template.hello.events({
+		'click button': function() {
+			// increment the counter when button is clicked
+			Session.set("counter", Session.get("counter") + 1);
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function() {
+
+		if (process.env.NODE_ENV === 'production') {
+			console.log('production');
+
+			var rootUrl = __meteor_runtime_config__.ROOT_URL;
+			BrowserPolicy.content.allowConnectOrigin(rootUrl);
+			BrowserPolicy.content.allowConnectOrigin(rootUrl.replace('http', 'ws'));
+
+		} else if (process.env.IS_MIRROR) {
+			console.log('testing');
+
+			var localUrl = 'http://localhost:3000';
+			var mirrorUrl = 'http://localhost:5000';
+
+			console.log("In development mode. Allowing all framing so that mocha-web can run for tests.");
+			BrowserPolicy.content.allowOriginForAll("localhost:*");
+			BrowserPolicy.content.allowConnectOrigin("ws://localhost:5000");
+			BrowserPolicy.content.allowConnectOrigin("ws://localhost:3000");
+			BrowserPolicy.content.allowFrameOrigin(mirrorUrl);
+			//BrowserPolicy.framing.allowAll();
+		}
+
+	});
 }
